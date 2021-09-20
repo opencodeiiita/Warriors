17fbb314bc1b15e166a2fee7c6a9bf13328f17cc
Shreyas Gupta <technology.shreyas@gmail.com>
2021-09-20 12:10:02 +0530

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Warriors
+
+<center>
+<img src="https://cdn3.iconfinder.com/data/icons/fantasy-and-role-play-game-adventure-quest/512/Sword-128.png"/>
+</center>
+
+Warriors is a Web3 MMO Game built on Polygon. Generate your warriors and fight against the world!
---
+++ contracts/WarriorBase.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract WarriorBase {
+    uint dnaDigits = 10;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Warrior {
+        string name;
+        address owner;
+        uint warriorType;
+        uint dna;
+    }
+
+    // TODO: Define the 4 warrior types
+
+    Warrior[] public warriors;
+    mapping (address => uint[]) public ownerToWarriorIds;
+
+    function _createWarrior(string memory _name, uint _warriorType, uint _dna) private {
+        warriors.push(Warrior(_name, msg.sender, _warriorType, _dna));
+        uint id = warriors.length - 1;
+        ownerToWarriorIds[msg.sender].push(id);
+        // TODO: add a CreateWarrior event and emit it.
+    }
+
+    function _generateRandomDna(string memory _name, uint _warriorType) private view returns (uint) {
+        // TODO: generate random DNA number of length 10, using name and type as seed.
+    }
+
+    function createRandomWarrior(string memory _name, uint _warriorType) public {
+        require(_warriorType >= 0 && _warriorType <= 3);
+        uint randDna = _generateRandomDna(_name, _warriorType);
+        _createWarrior(_name, _warriorType, randDna);
+    }
+
+}
