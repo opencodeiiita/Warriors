0ca4dd1d7efec722c968eff3698ce95ce5276e26
Your Name <you@example.com>
2021-10-06 13:01:31 +0530

    Cross checked all the errors and formatted the file

--- contracts/WarriorBase.sol
+++ contracts/WarriorBase.sol
@@ -2,93 +2,62 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract WarriorBase {
-    uint256 dnaDigits = 10;
-    uint256 dnaModulus = 10**dnaDigits;
-
+    uint dnaDigits = 10;
+    uint dnaModulus = 10 ** dnaDigits;
 
     struct Warrior {
         string name;
-        string warriorId;
         address owner;
-        uint256 warriorType;
-        uint256 dna;
-        uint256 xp;
-        uint256 lastTrained;
-
+        uint warriorType;
+        uint dna;
+        uint xp;
+        uint lastTrained;
     }
 
-    string[] warriorClasses = ["Elephant", "Knight", "Archer", "Swordsman"];
+    string[] warriorClasses = ["Elephant", "Knight", "Archer","Swordsman"];
 
-    string[] elephantStates = [
-        "Battle Elephant",
-        "Elite Battle Elephant",
-        "Destroyer Elephant"
-    ];
+    string[] elephantStates = ["Battle Elephant", "Elite Battle Elephant", "Destroyer Elephant"];
     string[] knightStates = ["Knight", "Cavalier", "Paladin"];
-    string[] archerStates = ["Archer", "Crossbowman", "Arbalester"];
-    string[] swordsmanStates = [
-        "Long Swordsman",
-        "Twohanded Swordsman",
-        " Champion"
-    ];
+    string[] archerStates =["Archer", "Crossbowman", "Arbalester"];
+    string[] swordsmanStates =["Long Swordsman", "Twohanded Swordsman", " Champion"];
 
     Warrior[] public warriors;
-    mapping(address => uint256[]) public ownerToWarriorIds;
+    mapping (address => uint[]) public ownerToWarriorIds;
 
-    event NewWarrior(
-        string name,
-        address owner,
-        uint256 warriorType,
-        uint256 dna,
-        
+    event NewWarrior (
+    uint id,   
+    string name ,
+    address owner ,
+    uint warriorType,
+    uint dna
     );
 
-    function _createWarrior(
-        string memory _name,
-        uint256 _warriorType,
-        uint256 _dna
-    ) private {
-        warriors.push(Warrior(_name, msg.sender, _warriorType, _dna, 0, 0));
-        uint256 id = warriors.length - 1;
+    function _createWarrior(string memory _name, uint _warriorType, uint _dna) private {
+        warriors.push(Warrior(_name, msg.sender, _warriorType, _dna,0, 0));
+        uint id = warriors.length - 1;
         ownerToWarriorIds[msg.sender].push(id);
-
-        emit NewWarrior(_name, msg.sender, _warriorType, _dna, );
+        emit NewWarrior(id,_name, msg.sender, _warriorType, _dna ) ;
+    }
     }
 
-    function _generateRandomDna(string memory _name, uint256 _warriorType)
-        private
-        view
-        returns (uint256)
-    {
-        uint256 rand = uint256(
-            keccak256(abi.encodePacked(_warriorType, _name))
-        );
+    function _generateRandomDna(string memory _name, uint _warriorType) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_warriorType ,_name)));
         return rand % dnaModulus;
     }
 
-    function createRandomWarrior(string memory _name, uint256 _warriorType)
-        public
-    {
+    function createRandomWarrior(string memory _name, uint _warriorType) public {
         require(_warriorType >= 0 && _warriorType <= 3);
-        uint256 randDna = _generateRandomDna(_name, _warriorType);
+        uint randDna = _generateRandomDna(_name, _warriorType);
         _createWarrior(_name, _warriorType, randDna);
     }
-
-    function _separateWarriorType(uint256 _warriorType)
-        private
-        view
-        returns (uint256, uint256)
-    {
-        uint256 stateId = _warriorType % 10;
-        uint256 classId = (_warriorType - stateId) / 10;
+    
+    function _separateWarriorType(uint _warriorType) private view returns (uint, uint) {
+        uint stateId = _warriorType % 10;
+        uint classId = (_warriorType - stateId)/10;
         return (classId, stateId);
     }
-
-    function _returnWarriorClassAndName(uint256 _classId, uint256 _stateId)
-        private
-        view
-        returns (string memory, string memory)
-    {
+    
+    function _returnWarriorClassAndName(uint _classId, uint _stateId) private view returns (string memory, string memory) {
         string memory className = warriorClasses[_classId];
         string memory warriorName;
         if (_classId == 0) {
@@ -102,26 +71,23 @@ contract WarriorBase {
         }
         return (className, warriorName);
     }
-
-    function _getTimeDuration(uint256 _startTime, uint256 _endTime)
-        private
-        view
-        returns (uint256)
-    {
-        uint256 duration = _endTime - _startTime;
+    
+    function _getTimeDuration(uint _startTime, uint _endTime) private view returns (uint) {
+        uint duration = _endTime - _startTime;
         return duration;
     }
 
-    function trainWarrior(uint256 warriorId) public {
+    function trainWarrior(uint warriorId) public {
         require(
             warriorId >= 0 &&
-                warriorId <= warriors.length &&
-                warriors[warriorId].owner == msg.sender
+            warriorId <= warriors.length &&
+            warriors[warriorId].owner == msg.sender
         );
-        uint256 lastTrainedDuration = (
-            _getTimeDuration(warriors[warriorId].lastTrained, block.timestamp)
-        ) / 1 days;
-        require(lastTrainedDuration >= 1);
+        uint lastTrainedDuration = (_getTimeDuration(warriors[warriorId].lastTrained, block.timestamp))/ 1 days;
+        require(
+          lastTrainedDuration >= 1
+        );
         warriors[warriorId].xp += 100;
     }
+
 }
