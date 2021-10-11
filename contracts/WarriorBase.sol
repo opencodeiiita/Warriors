0c5c84c88ffcb68b6b6eeee378ca7cf3f3b72d83
Shreyas Gupta <technology.shreyas@gmail.com>
2021-10-11 12:47:20 +0530

    Update WarriorBase.sol

--- contracts/WarriorBase.sol
+++ contracts/WarriorBase.sol
@@ -172,20 +172,13 @@ contract WarriorBase {
         warriors[warriorId].xp += 100;
         warriors[warriorId].lastTrained = block.timestamp;
         emit WarriorTrained(warriorId, warriors[warriorId].name, msg.sender, warriors[warriorId].xp, warriors[warriorId].warriorType, warriors[warriorId].dna, warriors[warriorId].lastTrained);
-        if(warriors[warriorId].xp == 1000)
-        {
-            _updateWarriorType(warriorId);
-        }
-        else if(warriors[warriorId].xp == 3000)
-        {
-            _updateWarriorType(warriorId);
+        if(warriors[warriorId].xp == 1000 || warriors[warriorId].xp == 3000) {
+            _levelUpWarrior(warriorId);
         }
     }
-    function _updateWarriorType(uint warriorId) private {
-
+    function _levelUpWarrior(uint warriorId) private {
         warriors[warriorId].warriorType++;
-
     }
 
-    }
-      
+}
+     
